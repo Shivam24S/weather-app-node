30488174c29bd1b11e94455356d808d4548159fd
Shivam24S <shivamshrivastav8055@gmail.com>
2024-06-12 12:48:19 +0530

    geocoding for getting lat and lon using open street map

--- app.js
+++ app.js
@@ -41,3 +41,35 @@ request({ url, json: true }, (error, response, body) => {
     }
   }
 });
+
+// const request = require('postman-request');
+
+const location = "Los Angeles";
+const url2 = `https://nominatim.openstreetmap.org/search?q=${encodeURIComponent(
+  location
+)}&format=json`;
+
+// using geocoding feature for getting latitude and longitude data using place name
+
+request(
+  { url: url2, json: true, headers: { "User-Agent": "request" } },
+  (error, response, body) => {
+    if (error) {
+      console.log("Can't fetch geocoding data:", error);
+    } else if (response.statusCode !== 200) {
+      console.log(
+        `Failed to fetch geocoding data. Status code: ${response.statusCode}`
+      );
+    } else {
+      console.log("Geocoding Data:", body[0]);
+      if (body && body.length > 0) {
+        const latitude = body[0].lat;
+        console.log("latitude", latitude);
+        const longitude = body[0].lon;
+        console.log("longitude", longitude);
+      } else {
+        console.log("No geocoding data available.");
+      }
+    }
+  }
+);
