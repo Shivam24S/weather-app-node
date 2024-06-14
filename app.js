467fcd59696067f1338b7a6984d4742757f20adf
Shivam24S <shivamshrivastav8055@gmail.com>
2024-06-14 13:12:00 +0530

    setting location using argv

--- app.js
+++ app.js
@@ -1,24 +1,42 @@
 const geocoding = require("./util/geocoding.js");
 const weather = require("./util/weather.js");
 
-// getting geocoding data
+const yargs = require("yargs/yargs");
+const { hideBin } = require("yargs/helpers");
 
-geocoding("ahmadabad", (data, error) => {
-  if (error) {
-    console.log("Error:", error);
-  } else {
-    console.log("Geocoding Data:", {
-      placeName: data.placeName,
-      latitude: data.latitude,
-      longitude: data.longitude,
+yargs(hideBin(process.argv)).command({
+  command: "name",
+  describe: "Specify location name",
+  builder: {
+    name: {
+      describe: "Location name",
+      demandOption: true,
+      type: "string",
+    },
+  },
+  handler(argv) {
+    let locationName = argv.name;
+
+    // getting geocoding data
+
+    geocoding(locationName, (data, error) => {
+      if (error) {
+        console.log("Error:", error);
+      } else {
+        console.log("Geocoding Data:", {
+          placeName: data.placeName,
+          latitude: data.latitude,
+          longitude: data.longitude,
+        });
+      }
+      // getting wether data
+      weather(data.latitude, data.longitude, (data, error) => {
+        if (error) {
+          console.log(error);
+        } else {
+          console.log("weather data", data);
+        }
+      });
     });
-  }
-  // getting wether data
-  weather(data.latitude, data.longitude, (data, error) => {
-    if (error) {
-      console.log(error);
-    } else {
-      console.log("weather data", data);
-    }
-  });
-});
+  },
+}).argv;
